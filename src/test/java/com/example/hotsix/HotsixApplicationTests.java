41199069b6096339b81199638cd851dd71bfeea8
임동길 <person456@naver.com>
2024-07-15 15:35:15 +0900

    init project

---
+++ src/test/java/com/example/hotsix/HotsixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotsix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotsixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
