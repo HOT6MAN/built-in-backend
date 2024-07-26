422751b0ec0b9db185a0f6660e59a694cd419057
임동길 <person456@naver.com>
2024-07-26 15:23:38 +0900

    fix: contextLoad() 주석처리

--- src/test/java/com/example/hotsix/HotsixApplicationTests.java
+++ src/test/java/com/example/hotsix/HotsixApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HotsixApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
