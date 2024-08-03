0142d1127cedfbea86046aab4793fd7081944c31
김노아 <noah0504@naver.com>
2024-08-03 20:13:26 +0900

    BE-FM-S11P12A606-220-2: recruits()

--- src/main/java/com/example/hotsix/controller/recruit/RecruitController.java
+++ src/main/java/com/example/hotsix/controller/recruit/RecruitController.java
@@ -1,6 +1,7 @@
 package com.example.hotsix.controller.recruit;
 
 import com.example.hotsix.dto.recruit.RecruitResponse;
+import com.example.hotsix.dto.recruit.RecruitShortResponse;
 import com.example.hotsix.editor.RecruitPropertyEditor;
 import com.example.hotsix.model.Recruit;
 import com.example.hotsix.service.recruit.RecruitService;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.inject.Provider;
+
+import java.util.List;
 
 @RestController
 public class RecruitController {
@@ -35,5 +39,24 @@ public class RecruitController {
         recruitService.save(recruit);
 
         return recruitService.toResponse(recruit);
+    }
+
+    @GetMapping("/teambuilding/recruits")
+    public List<RecruitShortResponse> recruits(
+            @RequestParam(value = "teamName", required = false) String teamName,
+            @RequestParam(value = "authorName", required = false) String authorName,
+            @RequestParam(value = "desiredPos", required = false) String desiredPos
+    ) throws InterruptedException {
+        Thread.sleep(1000*3);
+
+        if (teamName != null) {
+            return recruitService.findSummaryAllByTeamName(teamName);
+        } else if (authorName != null) {
+            return recruitService.findSummaryAllByAuthorName(authorName);
+        } else if (desiredPos != null) {
+            return recruitService.findSummaryAllByDesiredPos(desiredPos);
+        }
+
+        return recruitService.findSummaryAll();
     }
 }
