dda10c91f2b7f462f611893e808ab555c86b8940
김도훈 <k_dh610@naver.com>
2024-08-07 08:50:01 +0900

    fix: mail link

--- src/main/java/com/example/hotsix/service/auth/MailLinkService.java
+++ src/main/java/com/example/hotsix/service/auth/MailLinkService.java
@@ -28,6 +28,8 @@ public class MailLinkService {
     public String createLink(String type, String email){
         String code = jwtUtil.createEmailJwt(email);
         log.info(code);
+        //링크는 서버주소로 해야함. 서버의 /register나 /email-login api로 들어와서 code의 토큰을 검증해줘야
+        // 클라이언트의 /afterlogin이나 /register 페이지로 보냄.
         String link = hostUrl+"/hot6man/"+ type + "?code="+code;
 
         return link;
