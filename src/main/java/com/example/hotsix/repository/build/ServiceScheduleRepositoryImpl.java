d3db9ab4657c6abe5cae48a0ad95f060ba62f605
임동길 <person456@naver.com>
2024-08-06 15:02:01 +0900

    feat: 컨테이너 이전 로그 및 실시간 로그 조회 기능 구현

--- src/main/java/com/example/hotsix/repository/build/ServiceScheduleRepositoryImpl.java
+++ src/main/java/com/example/hotsix/repository/build/ServiceScheduleRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.example.hotsix.repository.build;
 
+import com.example.hotsix.model.ServiceSchedule;
+import com.example.hotsix.model.project.TeamProjectInfo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.example.hotsix.model.QServiceSchedule.serviceSchedule;
+import static com.example.hotsix.model.QTeam.team;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,11 +17,24 @@ public class ServiceScheduleRepositoryImpl implements ServiceScheduleRepositoryC
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Long findEmptyService() {
+    public ServiceSchedule findEmptyService() {
         return queryFactory.selectFrom(serviceSchedule)
-                .where(serviceSchedule.isUsed.eq(false))
+                .where(serviceSchedule.isUsed.eq(false).and(
+                        serviceSchedule.isPendding.eq(false)
+                ))
                 .orderBy(serviceSchedule.id.asc())
                 .limit(1)
-                .fetchOne().getId();
+                .fetchOne();
+    }
+
+    @Override
+    public List<TeamProjectInfo> findUsedProjectInfoIdByTeamId(Long teamId) {
+        return queryFactory
+                .select(serviceSchedule.teamProjectInfo)
+                .from(serviceSchedule)
+                .join(serviceSchedule.team, team)
+                .where(team.id.eq(teamId)
+                        .and(serviceSchedule.isUsed.isTrue()))
+                .fetch();
     }
 }
