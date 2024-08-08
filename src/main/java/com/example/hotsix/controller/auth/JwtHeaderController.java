0b8ba4df9f340cbaec9a75c35d2b2b2317d66c76
김도훈 <k_dh610@naver.com>
2024-08-08 13:22:40 +0900

    fix: JwtHeadderControoler, EmailControlelr

--- src/main/java/com/example/hotsix/controller/auth/JwtHeaderController.java
+++ src/main/java/com/example/hotsix/controller/auth/JwtHeaderController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,24 +21,24 @@ import static com.example.hotsix.enums.Process.*;
 public class JwtHeaderController {
 //    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
     @GetMapping("/convert")
-    public ResponseEntity<?> convertJwtHeader(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> convertJwtHeader(HttpServletRequest request, HttpServletResponse response, @CookieValue(value = "access", required = false) Cookie accessCookie) throws IOException {
         log.info("/convert 컨트롤러");
-        String access = null;
-        Cookie[] cookies = request.getCookies();
+        String access = accessCookie.getValue();
 
-        for (Cookie cookie : cookies) {
-            if(cookie.getName().equals("access")){
-                log.info("cookie access {}",cookie.getValue() );
-                access = cookie.getValue();
-
-                //리프레시 토큰 쿠키 값 0
-                Cookie newCookie = new Cookie("access",null);
-                cookie.setMaxAge(0);
-                cookie.setPath("/");
-                response.addCookie(cookie);
-                break;
-            }
-        }
+//        Cookie[] cookies = request.getCookies();
+//        for (Cookie cookie : cookies) {
+//            if(cookie.getName().equals("access")){
+//                log.info("cookie access {}",cookie.getValue() );
+//                access = cookie.getValue();
+//
+//                //리프레시 토큰 쿠키 값 0
+//                Cookie newCookie = new Cookie("access",null);
+//                cookie.setMaxAge(0);
+//                cookie.setPath("/");
+//                response.addCookie(cookie);
+//                break;
+//            }
+//        }
         if (access != null) {
             // 액세스 토큰을 로컬 스토리지에 저장하기 위해 헤더에 포함
             response.setHeader("Authorization", "Bearer " + access);
