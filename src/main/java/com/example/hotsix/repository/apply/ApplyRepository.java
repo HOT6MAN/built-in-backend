9272ce563986f427d72e946bb92e5c454e043fa4
김노아 <noah0504@naver.com>
2024-08-09 10:48:21 +0900

    BE-FM-S11P12A606-274-1: add repository (apply)

---
+++ src/main/java/com/example/hotsix/repository/apply/ApplyRepository.java
@@ -0,0 +1,10 @@
+package com.example.hotsix.repository.apply;
+
+import com.example.hotsix.model.Apply;
+import com.example.hotsix.model.id.ApplyId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplyRepository extends JpaRepository<Apply, ApplyId> {
+}
